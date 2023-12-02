4255ff01678a5c53bf83534310e118ca91832658
Yoonsoo Kim <laptise@live.jp>
2023-12-03 03:43:06 +0900

    save is model

--- apilib/util/__init__.py
+++ apilib/util/__init__.py
@@ -1,4 +1,9 @@
 import subprocess
+import os
 
 def run_cli(args:str):
     subprocess.call(args.split())
+
+def is_model(name:str)->bool:
+    file_name, ext = os.path.splitext(name)
+    return ext in ['.safetensors', '.ckpt']
