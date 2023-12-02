3c82e24a5f41ef18f2db91a1c5d7df82152d5df6
Yoonsoo Kim <laptise@live.jp>
2023-12-03 00:02:25 +0900

    add shell flag

--- apilib/util/__init__.py
+++ apilib/util/__init__.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def run_cli(args):
-  with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+  with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True) as process:
     for line in process.stdout:
       print(line.decode('utf8'))
