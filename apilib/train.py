2b75e8f04f8e03e1f75d9d77ab2e206c9bab3873
Yoonsoo Kim <laptise@live.jp>
2023-12-02 16:28:00 +0900

    launch cmd

--- apilib/train.py
+++ apilib/train.py
@@ -220,7 +220,7 @@ def train_lora_xl(base_path:str,
     )
 
     args = gen_train_lora_args(output_config=output_config, train_config=train_config, sample_config=sampler_config, optimizer_config=AdamW8bitConfig())
-    cmd = f"accelerate launch --mixed_precision bf16 {sd_scripts_path}/train_network.py {args}"
+    cmd = f"python -m accelerate launch --mixed_precision bf16 {sd_scripts_path}/train_network.py {args}"
     print(f"Going to run {cmd}")
     run_cli(cmd.split())
     print("Training done!")
