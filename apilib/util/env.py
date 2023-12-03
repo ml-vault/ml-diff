fd315938f9033125197a5f3344e6298de8e09331
Yoonsoo Kim <laptise@live.jp>
2023-12-03 13:29:46 +0900

    upd modeld dir

--- apilib/util/env.py
+++ apilib/util/env.py
@@ -11,6 +11,6 @@ R_TOKEN = getenv("R_TOKEN")
 W_TOKEN = getenv("W_TOKEN")
 HF_USER = getenv("HF_USER")
 SKIP_PROC = getenv("SKIP_PROC")
-MODEL_DIR = getenv("MODEL_DIR")
+MODEL_DIR = "./_models"
 
 SDXL = f"{MODEL_DIR}/sd_xl_base_1.0.safetensors"
