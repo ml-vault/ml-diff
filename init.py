82eb3e9030200318c4240b10389ada91007bf83e
Yoonsoo Kim <laptise@live.jp>
2023-12-03 13:23:13 +0900

    download when start

---
+++ init.py
@@ -0,0 +1,5 @@
+from apilib.download import download_from_hf
+from apilib.util.env import MODEL_DIR
+
+download_from_hf("stabilityai/stable-diffusion-xl-base-1.0", "sd_xl_base_1.0.safetensors", MODEL_DIR)
+print("donwload SDXL completed!")
