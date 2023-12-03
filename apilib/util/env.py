5f6aae123bdb655e54f4b39513a855ff0eb0b440
Yoonsoo Kim <laptise@live.jp>
2023-12-03 13:23:23 +0900

    upd env

--- apilib/util/env.py
+++ apilib/util/env.py
@@ -11,3 +11,6 @@ R_TOKEN = getenv("R_TOKEN")
 W_TOKEN = getenv("W_TOKEN")
 HF_USER = getenv("HF_USER")
 SKIP_PROC = getenv("SKIP_PROC")
+MODEL_DIR = getenv("MODEL_DIR")
+
+SDXL = f"{MODEL_DIR}/sd_xl_base_1.0.safetensors"
