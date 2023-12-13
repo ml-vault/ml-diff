5b9d827fbe33db1f4cc3e56fcbf03dc3bce83193
Yoonsoo Kim <laptise@live.jp>
2023-12-13 20:24:40 +0900

    fix model dir

--- init.py
+++ init.py
@@ -1,5 +1,7 @@
+import os
 from apilib.download import download_from_hf
-from apilib.util.env import MODEL_DIR, R_TOKEN, W_TOKEN
+from apilib.util.env import MODEL_DIR
 
+os.makedirs(MODEL_DIR, exist_ok=True)
 download_from_hf("stabilityai/stable-diffusion-xl-base-1.0", "sd_xl_base_1.0.safetensors", MODEL_DIR)
 print("donwload SDXL completed!")
