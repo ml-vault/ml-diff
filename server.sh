1068b73d84064aecc5b61505e62c35242087e5f0
Yoonsoo Kim <laptise@live.jp>
2023-12-13 12:28:15 +0900

    copy runpod

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 mlvcli config -r $R_TOKEN -w $W_TOKEN 
 echo "mixed_precision: '$MIXED_PRECISION'" >> runpod.yaml
 echo "set auth config completed!"
-RUN cp runpod.yaml /root/.cache/huggingface/accelerate/default_config.yaml
+cp runpod.yaml /root/.cache/huggingface/accelerate/default_config.yaml
 python server.py
